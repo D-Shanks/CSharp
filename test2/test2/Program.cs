9578c0360532a3d07656588e3e4e829dfb210ad5
D-Shanks <917006037@qq.com>
2018-10-16 23:11:36 +0800

    v2

---
+++ test2/test2/Program.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"D:\Program Files\VS2017_workplace\C#_Git_Work\CSharp\test2\科研细则.docx";
+            using (WordprocessingDocument wordprocessingDocument =
+                WordprocessingDocument.Open(fileName, false))
+            {
+                // Create a Body object.
+                DocumentFormat.OpenXml.Wordprocessing.Body body =
+                    wordprocessingDocument.MainDocumentPart.Document.Body;
+                
+                // Get the Run elements after the specified element.
+                Console.WriteLine("Run elements after the first child are: ");
+                foreach (var paragraph in body.Elements())
+                {
+                    Console.WriteLine(paragraph.InnerText);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
